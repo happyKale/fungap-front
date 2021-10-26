88dbf28474fb3b4379be96fe69d1f4b840c04729
Asher <grit.developer.asher@gmail.com>
2021-10-26 22:41:10 +0900

    Add. 회원가입 api

---
+++ src/shared/apis.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { getToken, setToken, delToken } from './token';
+
+const instance = axios.create({
+  baseURL: '#',
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(config => {
+  config.headers['Content-Type'] = 'application/json; charset=utf-8';
+  config.headers['X-Requested-With'] = 'XMLHttpRequest';
+  config.headers['authorization'] = getToken();
+  config.headers.Accept = 'application/json';
+  return config;
+});
+
+const apis = {
+  // 로그인
+  signup: userinfo => instance.post('/user/signup', userinfo),
+  checkEmail: email => instance.post('/user/email_check', email),
+  checkNickname: nickname => instance.post('/user/nickname_check', nickname),
+};
+
+export default apis;
