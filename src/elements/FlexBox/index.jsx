192cf07d1d98f573c42175490839937fd1a609a3
Asher <grit.developer.asher@gmail.com>
2021-10-26 22:38:39 +0900

    Edit. 최소단위 컴포넌트 모음

--- src/elements/FlexBox/index.jsx
+++ src/elements/FlexBox/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import FlexBoxStyle from './style';
 
 const FlexBox = ({ children, ...rest }) => {
--- src/elements/index.jsx
+++ src/elements/index.jsx
@@ -2,5 +2,7 @@ import Button from './Button';
 import Input from './Input';
 import Select from './Select';
 import Option from './Option';
+import Form from './Form';
+import FlexBox from './FlexBox';
 
-export { Button, Input, Select, Option };
+export { Button, Form, Input, Select, Option, FlexBox };
