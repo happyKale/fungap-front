912039eba2b46d1ff60336979c24d9ac8c19adee
Asher <grit.developer.asher@gmail.com>
2021-10-26 22:42:26 +0900

    Add. 세션스토리지에서 토큰 관리하는 함수들

---
+++ src/shared/token.js
@@ -0,0 +1,21 @@
+const getToken = () => {
+  const token = sessionStorage.getItem('token');
+  if (token) {
+    return `Bearer ${token}`;
+  } else {
+    return null;
+  }
+};
+
+const setToken = token => {
+  if (!token) {
+    return false;
+  }
+  sessionStorage.setItem('token', token);
+};
+
+const delToken = () => {
+  sessionStorage.removeItem('token');
+};
+
+export { getToken, setToken, delToken };
