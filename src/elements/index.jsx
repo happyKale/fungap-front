3de3a022308e2d7cc2768597353afc8f1d260e0a
Asher <grit.developer.asher@gmail.com>
2021-10-27 11:51:15 +0900

    Fix. import 중복 제거

--- src/elements/index.jsx
+++ src/elements/index.jsx
@@ -5,6 +5,5 @@ import Text from './Text';
 import Select from './Select';
 import Option from './Option';
 import Form from './Form';
-import FlexBox from './FlexBox';
 
 export { Button, Form, Input, Select, Option, Text, FlexBox };
