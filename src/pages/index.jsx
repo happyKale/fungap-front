f3d970b00d5ea3c4a9e28dea8207ef523de65663
happyKale <syk767@gmail.com>
2021-11-02 03:43:52 +0900

    Add. adminPostManage페이지 추가

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,5 +4,18 @@ import GoogleSignIn from './GoogleSignIn';
 import KakaoSignIn from './KakaoSignIn';
 import NaverSignIn from './NaverSignIn';
 import Home from './Home';
+import Admin from './Admin';
+import AdminPostManage from './AdminPostManage';
+import AdminPostPreview from './AdminPostPreview';
 
-export { SignIn, SignUp, KakaoSignIn, NaverSignIn, GoogleSignIn, Home };
+export {
+  SignIn,
+  SignUp,
+  KakaoSignIn,
+  NaverSignIn,
+  GoogleSignIn,
+  Home,
+  Admin,
+  AdminPostManage,
+  AdminPostPreview,
+};
--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -2,7 +2,13 @@ import { ConnectedRouter } from 'connected-react-router';
 import { Route } from 'react-router';
 
 import { history } from '../redux/configureStore';
-import { SignUp, SignIn } from '../pages';
+import {
+  SignUp,
+  SignIn,
+  Admin,
+  AdminPostManage,
+  AdminPostPreview,
+} from '../pages';
 
 function App() {
   return (
@@ -10,6 +16,9 @@ function App() {
       <ConnectedRouter history={history}>
         <Route path='/signup' exact component={SignUp} />
         <Route path='/signin' exact component={SignIn} />
+        <Route path='/admin' exact component={Admin} />
+        <Route path='/admin_write' exact component={AdminPostManage} />
+        <Route path='/admin_preview' exact component={AdminPostPreview} />
       </ConnectedRouter>
     </>
   );
