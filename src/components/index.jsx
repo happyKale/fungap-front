35eaa51c2b94b111cc1e67bd091f2541a8bcacf3
Asher <grit.developer.asher@gmail.com>
2021-11-02 01:02:41 +0900

    Add. 댓글 갯구와 조회수를 나타내는 카운터 컴포넌트

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,5 +2,18 @@ import TypeOfMbti from './TypeOfMbti';
 import TopPost from './TopPost';
 import Post from './Post';
 import Navigation from './Navigation';
+import Comments from './Comments';
+import Comment from './Comment';
+import Counter from './Counter';
+import Goback from './Goback';
 
-export { TypeOfMbti, TopPost, Post, Navigation };
+export {
+  TypeOfMbti,
+  TopPost,
+  Post,
+  Navigation,
+  Comments,
+  Comment,
+  Counter,
+  Goback,
+};
