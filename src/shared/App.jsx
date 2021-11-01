cb42c82246e2c5037feaf717381eb3c5b46e5069
Asher <grit.developer.asher@gmail.com>
2021-11-02 01:11:25 +0900

    Style. 가장 바깥 컨테이너의 스타일 추가

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -2,16 +2,23 @@ import { ConnectedRouter } from 'connected-react-router';
 import { Route } from 'react-router';
 
 import { history } from '../redux/configureStore';
-import { SignUp, SignIn } from '../pages';
+import { SignUp, SignIn, Detail } from '../pages';
+import { Navigation } from '../components';
+
+import style from './App.module.css';
 
 function App() {
   return (
-    <>
-      <ConnectedRouter history={history}>
-        <Route path='/signup' exact component={SignUp} />
-        <Route path='/signin' exact component={SignIn} />
-      </ConnectedRouter>
-    </>
+    <div className={style.container}>
+      <div>
+        {/* <Navigation /> */}
+        <ConnectedRouter history={history}>
+          <Route path='/signup' exact component={SignUp} />
+          <Route path='/signin' exact component={SignIn} />
+          <Route path='/detail' exact component={Detail} />
+        </ConnectedRouter>
+      </div>
+    </div>
   );
 }
 
