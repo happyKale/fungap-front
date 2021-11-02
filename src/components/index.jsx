0e4f2f91106bc37e48bf61c02e80cf936f283196
Asher <grit.developer.asher@gmail.com>
2021-11-02 19:22:41 +0900

    Add. 게시글 분류 컴포넌트

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,14 +1,16 @@
+import Navigation from './Navigation';
 import TypeOfMbti from './TypeOfMbti';
 import TopPost from './TopPost';
-import Post from './Post';
-import Navigation from './Navigation';
 import PostList from './PostList';
+import Post from './Post';
 import Comments from './Comments';
 import Comment from './Comment';
 import CommentInput from './CommentInput';
 import Counter from './Counter';
 import Goback from './Goback';
 import Chat from './Chat';
+import SearchBar from './SearchBar';
+import SortContents from './SortContents';
 
 export {
   TypeOfMbti,
@@ -22,4 +24,6 @@ export {
   Counter,
   Goback,
   Chat,
+  SearchBar,
+  SortContents,
 };
