b2a7592da0ac5158c22b0962f02a5b6936019267
Asher <grit.developer.asher@gmail.com>
2021-11-02 19:27:26 +0900

    Edit. 소스코드를 간단하게 수정

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import heart from '../../assets/heart.png';
 import style from './post.module.css';
-const Post = ({ children, ...rest }) => {
-  const direction = rest.direction;
-
+const Post = ({
+  board_id,
+  board_title,
+  board_image,
+  view_count,
+  comment_count,
+  like_count,
+  like_state,
+  direction,
+}) => {
   return (
     <div className={direction === 'row' ? style.rowFlex : style.columnFlex}>
       <img
         className={direction === 'row' ? style.rowImg : style.columnImg}
-        src={rest.board_image}
+        src={board_image}
         alt='임시이미지'
       />
       <div className={style.textContent}>
-        <p className={style.title}>{rest.board_title}</p>
-
+        <p className={style.title}>{board_title}</p>
         <div className={style.content}>
-          <p className={style.viewCount}>조회수 {rest.view_count}</p>
-
-          <p className={style.commentCount}>댓글 {rest.comment_count}</p>
+          <p className={style.viewCount}>조회수 {view_count}</p>
+          <p className={style.commentCount}>댓글 {comment_count}</p>
         </div>
         <button className={style.heartButton}>
           <img className={style.heartImage} src={heart} alt='좋아요' />
-          {rest.like_count}
+          {like_count}
         </button>
       </div>
     </div>
