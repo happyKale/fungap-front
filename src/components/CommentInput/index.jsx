4ad72610baf57b0d49c82966f3206dcda13f564a
Asher <grit.developer.asher@gmail.com>
2021-11-02 14:40:06 +0900

    Add. 댓글 입력

---
+++ src/components/CommentInput/index.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { commentActions } from '../../redux/modules/comment';
+import style from './commentInput.module.css';
+
+const CommentInput = props => {
+  const dispatch = useDispatch();
+  const [input, setInput] = useState({ comment: '' });
+  const { comment } = input;
+  //
+  const handleChange = e => {
+    const { value, name } = e.target;
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+    console.log(name, value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // dispatch(commentActions.addCommentDB(boardId, comment));
+    setInput({
+      comment: '',
+    });
+  };
+
+  return (
+    <form //
+      className={style.inputWrap}
+      onClick={handleSubmit}
+    >
+      <label //
+        className={style.label}
+        htmlFor=''
+      ></label>
+      <input //
+        name='comment'
+        value={comment}
+        className={style.input}
+        type='text'
+        placeholder='댓글을 입력해주세요.'
+        onChange={handleChange}
+      />
+      <button className={style.write}>작성</button>
+    </form>
+  );
+};
+
+export default CommentInput;
