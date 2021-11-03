f1d290ac931d5055d103204b995645d15b95f760
Ldonggun <89460880+Ldonggun@users.noreply.github.com>
2021-11-03 22:14:37 +0900

    Edit. useredit 수정부분

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -4,6 +4,8 @@ import { useSelector } from 'react-redux';
 import { history } from '../../redux/configureStore';
 const Chat = ({ ...rest }) => {
   const isLogin = useSelector(state => state.user.is_login);
+  const userInfo = JSON.parse(sessionStorage.getItem('user'));
+  console.log(userInfo);
   const goProfileEdit = () => {
     history.push('/useredit');
   };
@@ -13,17 +15,21 @@ const Chat = ({ ...rest }) => {
         <img
           className={style.userImage}
           alt='유저이미지'
-          src='https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg'
+          src={
+            userInfo?.user_image
+              ? userInfo.user_image
+              : 'https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg'
+          }
         />
         {isLogin ? (
           <div>
             <div className={style.userEdit}>
-              <h3>임동건님</h3>
+              <h3>{userInfo.nickname}님</h3>
               <button onClick={goProfileEdit}>수정</button>
             </div>
 
             <p>즐거운 시간 되세요!</p>
-            <span>INFJ</span>
+            <span>{userInfo.user_mbti}</span>
           </div>
         ) : (
           <div>
--- src/pages/ProfileEdit/index.jsx
+++ src/pages/ProfileEdit/index.jsx
@@ -2,19 +2,37 @@ import React from 'react';
 import style from './profileedit.module.css';
 import userPlaceholer from '../../assets/userplaceholder.png';
 import { Goback } from '../../components';
+import { useDispatch } from 'react-redux';
+import { userActions } from '../../redux/modules/user';
 const ProfileEdit = () => {
+  const dispatch = useDispatch();
+  const userInfo = JSON.parse(sessionStorage.getItem('user'));
+
   const [previewImg, setPreviewImg] = React.useState(userPlaceholer);
+  const [nickname, setNickname] = React.useState(userInfo?.nickname);
+  const [user_mbti, setUser_mbti] = React.useState(userInfo.user_mbti);
+
+  const changeNickname = e => {
+    setNickname(e.target.value);
+  };
+  const changeMbti = e => {
+    setUser_mbti(e.target.value);
+  };
 
   const onChangeHandle = e => {
     const reader = new FileReader();
     const file = e.target.files[0];
-
     reader.readAsDataURL(file);
     reader.onloadend = () => {
       setPreviewImg(reader.result);
     };
   };
-
+  const user_image =
+    'https://img.huffingtonpost.com/asset/5dd5f293250000ab11d2dbc4.jpeg?cache=A1ADNLUVMY&ops=scalefit_630_noupscale';
+  const newinfo = { user_mbti, nickname, user_image };
+  const updateUserInfo = () => {
+    dispatch(userActions.updateUserInfoDB(newinfo));
+  };
   const mbtiList = [
     'ISTJ',
     'ISFJ',
@@ -50,15 +68,17 @@ const ProfileEdit = () => {
       <img src={previewImg} alt='유저이미지' className={style.userImage} />
       <div className={style.inputContent}>
         <p>닉네임</p>
-        <input type='text' placeholder=' 닉네임을 입력해주세요' />
+        <input type='text' value={nickname} onChange={changeNickname} />
         <p>MBTI</p>
-        <select>
+        <select value={user_mbti} onChange={changeMbti}>
           {mbtiList.map((list, index) => {
             return <option key={index}>{list}</option>;
           })}
         </select>
       </div>
-      <div className={style.editBtn}>수정</div>
+      <div className={style.editBtn} onClick={updateUserInfo}>
+        수정
+      </div>
     </div>
   );
 };
