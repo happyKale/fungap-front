6c0fffdc997c76b23310acb0bdf26cc98e9cde80
happyKale <syk767@gmail.com>
2021-11-04 18:30:43 +0900

    Add. post, editPost 수정 액션 추가, 이미지 추가 액션 추가.

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -10,6 +10,11 @@ const SET_POST = 'SET_POST';
 const ADD_POST = 'ADD_POST';
 const DELETE_POST = 'DELETE_POST';
 const EDIT_POST = 'EDIT_POST';
+const ADD_IMAGE = 'ADD_IMAGE';
+const RESET_POST = 'RESET_POST';
+const RESET_EDIT_POST = 'RESET_EDIT_POST';
+const SET_EDIT_POST = 'SET_EDIT_POST';
+const ADD_EDIT_POST = 'ADD_EDIT_POST';
 
 // action creator
 const getPosts = createAction(GET_POST, posts => ({ posts }));
@@ -22,9 +27,26 @@ const addPost = createAction(ADD_POST, (title, img, desc, mbti) => ({
 }));
 const deletePost = createAction(DELETE_POST, posts => ({ posts }));
 const editPost = createAction(EDIT_POST, (postId, post) => ({ postId, post }));
+const addImage = createAction(ADD_IMAGE, image => ({ image }));
+const resetPost = createAction(RESET_POST, () => ({}));
+const resetEditPost = createAction(RESET_EDIT_POST, () => ({}));
+const setEditPost = createAction(SET_EDIT_POST, postId => ({ postId }));
+const addEditPost = createAction(ADD_EDIT_POST, (title, img, desc, mbti) => ({
+  title,
+  img,
+  desc,
+  mbti,
+}));
 
 // initial state
 const initialState = {
+  editPost: {
+    board_title: '',
+    board_image: '',
+    board_desc: '',
+    board_content: '',
+  },
+  postImg: '',
   post: {
     title: '',
     img: '',
@@ -355,23 +377,18 @@ const getPostDB = isSort => {
 const addPostDB = () => {
   return (dispatch, getState, { history }) => {
     const post = getState().post.post;
+    const image = getState().post.postImg;
     apis
       .addPost({
         board_title: post.title,
         board_desc: post.desc,
-        board_image: post.img,
+        board_image: image,
         board_content: post.mbti,
       })
-      .then(
-        apis
-          .getPosts()
-          .then(res => {
-            dispatch(setPosts(res.board_list));
-          })
-          .then(() => {
-            history.push('/');
-          }),
-      )
+      .then(res => {
+        dispatch(resetPost());
+        history.push('/admin');
+      })
       .catch(error => {
         console.log(error);
       });
@@ -382,7 +399,16 @@ const deletePostDB = postId => {
   return (dispatch, getState, { history }) => {
     const postList = getState().post.postList;
     const resultList = postList.filter(post => post.board_id !== postId);
-    dispatch(deletePost(resultList));
+
+    apis
+      .deletePost(postId)
+      .then(res => {
+        console.log(res);
+        dispatch(deletePost(resultList));
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 };
 const editPostDB = (
@@ -399,14 +425,23 @@ const editPostDB = (
       board_desc,
       board_content,
     };
-    dispatch(editPost(postId, result));
-    history.push('/admin');
+    apis
+      .editPost(postId, result)
+      .then(res => {
+        dispatch(editPost(postId, result));
+        dispatch(resetEditPost());
+        history.push('/admin');
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 };
 const getAdminPostDB = () => {
   return (dispatch, getState, { history }) => {
     apis.getPosts().then(res => {
       console.log('어드민 데이터 가져오기: ', res.data);
+      dispatch(setPosts(res.data.board_list));
     });
   };
 };
@@ -443,6 +478,40 @@ export default handleActions(
           ...action.payload.post,
         };
       }),
+    [ADD_IMAGE]: (state, action) =>
+      produce(state, draft => {
+        draft.postImg = action.payload.image;
+      }),
+    [RESET_POST]: (state, action) =>
+      produce(state, draft => {
+        draft.post.title = '';
+        draft.post.img = '';
+        draft.post.desc = '';
+        draft.post.mbti = '';
+      }),
+    [RESET_EDIT_POST]: (state, action) =>
+      produce(state, draft => {
+        draft.editPost.board_title = '';
+        draft.editPost.board_image = '';
+        draft.editPost.board_desc = '';
+        draft.editPost.board_content = '';
+      }),
+    [SET_EDIT_POST]: (state, action) =>
+      produce(state, draft => {
+        let idx = draft.postList.findIndex(
+          post => post.board_id == action.payload.postId,
+        );
+        draft.editPost = {
+          ...draft.postList[idx],
+        };
+      }),
+    [ADD_EDIT_POST]: (state, action) =>
+      produce(state, draft => {
+        draft.editPost.board_title = action.payload.title;
+        draft.editPost.board_image = action.payload.img;
+        draft.editPost.board_desc = action.payload.desc;
+        draft.editPost.board_content = action.payload.mbti;
+      }),
   },
   initialState,
 );
@@ -452,6 +521,11 @@ export const postActions = {
   setPosts,
   editPost,
   addPost,
+  addImage,
+  resetPost,
+  resetEditPost,
+  setEditPost,
+  addEditPost,
   getPostDB,
   addPostDB,
   deletePostDB,
