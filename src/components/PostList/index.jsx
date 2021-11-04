fa81a1803bcc3c761ba2d0af00bc1dad303a8507
Asher <grit.developer.asher@gmail.com>
2021-11-04 22:00:53 +0900

    Fix. 관리자가 아닌 유저가 상황별 게시글 목록 불러오는 요청에러 예외처리

--- src/components/PostList/index.jsx
+++ src/components/PostList/index.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Post } from '../../components';
 import style from './postList.module.css';
 import { postActions } from '../../redux/modules/post';
 
-const PostList = props => {
-  console.log(props.isAdmin);
+const PostList = ({ isAdmin }) => {
   const dispatch = useDispatch();
   // 게시글 목록을 리덕스에서 가져온다.
   const postList = useSelector(state => state.post.postList);
   console.log('포스트 리스트: ', postList);
 
-  React.useEffect(() => {
+  useEffect(() => {
+    if (!isAdmin) {
+      return false;
+    }
     dispatch(postActions.getAdminPostDB());
   }, []);
 
   return (
     <div>
       {postList.map((post, index) => {
-        return (
-          <Post isAdmin={props.isAdmin} key={index} direction='row' {...post} />
-        );
+        return <Post isAdmin={isAdmin} key={index} direction='row' {...post} />;
       })}
     </div>
   );
