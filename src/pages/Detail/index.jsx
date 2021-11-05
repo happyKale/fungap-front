ec82e3c22a0d156cb251333f59e04bd09c685a48
Asher <grit.developer.asher@gmail.com>
2021-11-05 21:08:09 +0900

    Fix. 게시글 상세페이지 리로딩 시 오류 수정

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,43 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 
 import { Comments, Counter, Goback } from '../../components';
+import { postActions } from '../../redux/modules/post';
 import style from './detail.module.css';
 
 const Detail = props => {
+  const dispatch = useDispatch();
   const postList = useSelector(state => state.post.postList);
   const postId = parseInt(props.match.params.id);
-
-  const post = postList.find(item => {
+  const post = postList?.find(item => {
     return item.board_id === postId;
   });
 
-  const {
-    board_id,
-    board_title,
-    board_image,
-    like_count,
-    board_desc,
-    comment_count,
-    view_count,
-    like_state,
-  } = post;
+  useEffect(() => {
+    dispatch(postActions.getPostDB());
+  }, []);
 
   return (
     <>
       <div className={style.detail}>
-        <Goback page='/contents'>{board_title}</Goback>
+        <Goback page='/contents'>{post?.board_title}</Goback>
         <div className={style.image}>
-          <img src={board_image} alt={board_title} />
+          <img src={post?.board_image} alt={post?.board_title} />
         </div>
-        <h3 className={style.title}>{board_title}</h3>
-        <span className={style.like}>{like_count}</span>
-        <p className={style.contents}>{board_desc}</p>
+        <h3 className={style.title}>{post?.board_title}</h3>
+        <span className={style.like}>{post?.like_count}</span>
+        <p className={style.contents}>{}</p>
         <Counter //
-          commentCount={comment_count}
-          viewCount={view_count}
+          commentCount={post?.comment_count}
+          viewCount={post?.view_count}
         />
-        <Comments boardId={board_id} />
+        <Comments boardId={post?.board_id} />
       </div>
     </>
   );
