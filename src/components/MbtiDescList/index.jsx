4238adaaeb8ca29f2674e4a6c78842cf819b3e7c
happyKale <syk767@gmail.com>
2021-11-06 19:30:12 +0900

    Refactor. MbtiDescList 컴포넌트 수정

--- src/components/MbtiDescList/index.jsx
+++ src/components/MbtiDescList/index.jsx
@@ -3,24 +3,37 @@ import style from './mbtiDescList.module.css';
 
 const MbtiDescList = props => {
   const mbtiList = props.list;
+  const list = [
+    [mbtiList[0], mbtiList[1], mbtiList[2], mbtiList[3]],
+    [mbtiList[4], mbtiList[5], mbtiList[6], mbtiList[7]],
+    [mbtiList[8], mbtiList[9], mbtiList[10], mbtiList[11]],
+    [mbtiList[12], mbtiList[13], mbtiList[14], mbtiList[15]],
+  ];
+  console.log(list);
 
-  const sectionName = ['외교형', '탐험가형', '분석형', '관리자형'];
+  const sectionName = [
+    { name: '외교형', color: style.green },
+    { name: '탐험가형', color: style.yellow },
+    { name: '분석형', color: style.purple },
+    { name: '관리자형', color: style.blue },
+  ];
   return (
     <React.Fragment>
-      {mbtiList.map((mbti, idx) => {
-        let section = false;
-        const num = idx / 4;
-        if (num === 0 || num === 1 || num === 2 || num === 3) {
-          section = true;
-        }
+      {list.map((item, idx) => {
         return (
-          <React.Fragment>
-            {section && <div className={style.section}>{sectionName[num]}</div>}
-            <div key={idx} className={style.mbtiBox}>
-              <div className={style.mbtiName}>{mbti[0].toUpperCase()}</div>
-              <div className={style.mbtiDesc}>{mbti[1]}</div>
+          <div key={idx} className={style.container}>
+            <div className={sectionName[idx].color}>
+              {sectionName[idx]?.name}
             </div>
-          </React.Fragment>
+            {item.map(i => {
+              return (
+                <div key={i[0]} className={style.mbtiBox}>
+                  <div className={style.mbtiName}>{i[0]?.toUpperCase()}</div>
+                  <div className={style.mbtiDesc}>{i[1]}</div>
+                </div>
+              );
+            })}
+          </div>
         );
       })}
     </React.Fragment>
