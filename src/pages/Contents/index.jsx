4888432dcac97093bb7d2001c2eab3382dfb2a0d
Asher <grit.developer.asher@gmail.com>
2021-11-10 15:58:40 +0900

    Edit. 현재 저장된 게시글 갯수가 보이도록 수정

--- src/pages/Contents/index.jsx
+++ src/pages/Contents/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { Goback, PostList, SearchBar, SortContents } from '../../components';
+import { Goback, PostList, SortContents } from '../../components';
 import style from './contents.module.css';
 
 const Contents = props => {
+  const postList = useSelector(state => state.post.postList);
+
   return (
     <>
       <Goback page='/'>상황별 콘텐츠</Goback>
       <div className={style.wrapper}>
-        <span className={style.commentCount}>16개의 게시글</span>
+        <span className={style.commentCount}>{postList.length}의 게시글</span>
         <SortContents />
       </div>
       <PostList />
