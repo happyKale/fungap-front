d821a88bd8e2237e8a237dbecd87bc57222ac877
Asher <grit.developer.asher@gmail.com>
2021-11-11 00:09:43 +0900

    Edit. 컴포넌트 이름 변경 및 좋아요, 공유버튼 해당 컴포넌트로 이동

---
+++ src/components/ContentFt/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { LikeButton, KakaoShareButton } from '..';
+import style from './contentFt.module.css';
+
+const ContentFt = ({
+  boardId,
+  commentCount,
+  viewCount,
+  likeCount,
+  likeState,
+  postId,
+}) => {
+  return (
+    <div className={style.counter}>
+      <div className={style.counts}>
+        <span className={style.comments}>댓글 {commentCount}</span>
+        <span className={style.views}>조회수 {viewCount}</span>
+      </div>
+      <div className={style.buttons}>
+        <LikeButton
+          board_id={boardId}
+          like_count={likeCount}
+          like_state={likeState}
+        />
+        <KakaoShareButton postId={postId} />
+      </div>
+    </div>
+  );
+};
+
+export default ContentFt;
