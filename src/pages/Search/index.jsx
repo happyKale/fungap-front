24ab51a30489d0f5c26b19c6b2e34833e9b47097
Ldonggun <89460880+Ldonggun@users.noreply.github.com>
2021-11-11 14:05:15 +0900

    Feat. 서치페이지 추천검색어쪽 뷰 수정

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { history } from '../../redux/configureStore';
 import { postActions } from '../../redux/modules/post';
 import { Post, Goback, SearchBar } from '../../components';
 import style from './search.module.css';
@@ -20,19 +20,31 @@ const Search = props => {
       <Goback page='/'>검색</Goback>
       <SearchBar />
       <h3>펀캡 추천 검색어</h3>
-      <ul>
-        <li>
-          <button>MBTI 유형별 궁합</button>
-        </li>
-        <li>
-          <button>MBTI 유형별 반려견 추천</button>
-        </li>
-        <li>
-          <button>MBTI 유형별 무인도에 고립되었을 때</button>
-        </li>
-      </ul>
-      <p>펀갭 서비스 런칭!</p>
       <div>
+        <div className={style.recommendList}>
+          <div name='아이콘이미지' />
+          <p>MBTI 유형별 궁합</p>
+        </div>
+        <div className={style.recommendList}>
+          <div name='아이콘이미지' />
+          <p>MBTI 유형별 반려견 추천</p>
+        </div>
+        <div className={style.recommendList}>
+          <div name='아이콘이미지' />
+          <p>MBTI 유형별 무인도에 고립되었을 때</p>
+        </div>
+      </div>
+      <div className={style.list4}>
+        <div name='아이콘이미지' />
+        <p>펀갭 서비스 런칭!</p>
+      </div>
+
+      <div
+        className={style.banner}
+        onClick={() => {
+          history.push('/contents');
+        }}
+      >
         <h3>펀갭 콘텐츠 모두 보기</h3>
         <p>펀갭에 있는 재미있는 이야기를 모두 살펴보세요.</p>
       </div>
