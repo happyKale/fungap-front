9d2e041872dade0677a0d918be23c25bd63c9717
Asher <grit.developer.asher@gmail.com>
2021-11-13 00:03:24 +0900

    Add. 상황별게시글 검색바 추가

--- src/pages/Contents/index.jsx
+++ src/pages/Contents/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import { Goback, PostList, SortContents } from '../../components';
+import { Goback, PostList, SearchBar, SortContents } from '../../components';
 import style from './contents.module.css';
 
 const Contents = props => {
@@ -10,6 +10,7 @@ const Contents = props => {
   return (
     <>
       <Goback page='/'>상황별 콘텐츠</Goback>
+      <SearchBar />
       <div className={style.wrapper}>
         <span className={style.commentCount}>{postList.length}개의 게시글</span>
         <SortContents />
