361435ee8f9c61c43ca1db6016581a3194092140
Ldonggun <89460880+Ldonggun@users.noreply.github.com>
2021-11-16 17:32:54 +0900

    Fix. 슬라이드보이는 방식수정 및 클릭시 해당게시글로이동

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -2,44 +2,71 @@ import React from 'react';
 import style from './carousel.module.css';
 import './slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import bannerImage from '../../assets/topBanner.png';
-import bannerImage2 from '../../assets/topBanner2.png';
-import bannerImage3 from '../../assets/topBanner3.png';
-import bannerImage4 from '../../assets/topBanner4.png';
-import bannerImage5 from '../../assets/topBanner5.png';
-import bannerImage6 from '../../assets/topBanner6.png';
-
 import Slider from 'react-slick';
+import { history } from '../../redux/configureStore';
 
 const Carousel = () => {
   const settings = {
     dots: true,
     infinite: true,
     autoplay: true,
-    speed: 500,
+    speed: 600,
     slidesToShow: 1,
     slidesToScroll: 1,
     draggable: true,
-    fade: true,
   };
-  const bannerList = [
-    bannerImage,
-    bannerImage2,
-    bannerImage3,
-    bannerImage4,
-    bannerImage5,
-    bannerImage6,
-  ];
+
   return (
     <div>
       <Slider {...settings}>
-        {bannerList.map((list, index) => {
-          return (
-            <div key={index} className={style.imageBox}>
-              <img src={list} alt='배너이미지' className={style.img} />
-            </div>
-          );
-        })}
+        <div
+          className={style.imageBox}
+          onClick={() => {
+            history.push('/compatibility');
+          }}
+        >
+          <div className={style.img} />
+        </div>
+        <div
+          className={style.imageBox}
+          onClick={() => {
+            history.push('/detail/33');
+          }}
+        >
+          <div className={style.img2} />
+        </div>
+        <div
+          className={style.imageBox}
+          onClick={() => {
+            history.push('/detail/11');
+          }}
+        >
+          <div className={style.img3} />
+        </div>
+        <div
+          className={style.imageBox}
+          onClick={() => {
+            history.push('/detail/32');
+          }}
+        >
+          <div className={style.img4} />
+        </div>
+        <div
+          className={style.imageBox}
+          onClick={() => {
+            history.push('/detail/35');
+          }}
+        >
+          <div className={style.img5} />
+        </div>
+        <div
+          className={style.imageBox}
+          onClick={() => {
+            history.push('/detail/16');
+          }}
+        >
+          <div className={style.img6} />
+        </div>
       </Slider>
     </div>
   );
