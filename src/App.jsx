ecf185946cd2bab7c87af9a737adc806233c886c
Asher <grit.developer.asher@gmail.com>
2021-11-27 03:04:00 +0900

    Edit. App.jsx 파일 위치 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
 import { useEffect } from 'react';
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+import { userActions } from '@redux/modules/user';
+// route
 import { ConnectedRouter } from 'connected-react-router';
 import { Route } from 'react-router';
-import { useSelector, useDispatch } from 'react-redux';
-
-import { Canvas } from '../shared/canvas';
-import { userActions } from '../redux/modules/user';
-import { history } from '../redux/configureStore';
+import { history } from '@redux/configureStore';
+// pages
 import {
   Home,
   Contents,
   Detail,
+  Search,
   SignUp,
   SignIn,
-  EmailSignIn,
+  SignInEmail,
   FindPwd,
+  UserPage,
+  ProfileEdit,
   Admin,
   AdminPostManage,
   AdminPostPreview,
-  Setting,
-  ProfileEdit,
   Notification,
   Alarm,
-  TermsofUse,
+  TermsOfUse,
   Chatting,
   ChatRoom,
-  Search,
-  Compatibility,
-  CompatibilityResult,
-} from '../pages';
-import { Navigation, ScrollRestoration } from '../components';
+  MbtiTest,
+  MbtiTestResult,
+} from '@pages';
+// components
+import { Navigation, ScrollRestoration } from '@components';
+// shared
+import { Canvas } from '@shared/canvas';
+// css
 import style from './App.module.css';
-import logo from '../assets/logo_yellow.png';
+// images
+import logo from '@assets/logo_yellow.png';
 
 function App() {
   const dispatch = useDispatch();
@@ -39,22 +45,18 @@ function App() {
   useEffect(() => {
     dispatch(userActions.signinCheckDB());
 
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(
-      window.navigator.userAgent,
-    );
+    // const isMobile = /iPhone|iPad|iPod|Android/i.test(
+    //   window.navigator.userAgent,
+    // );
 
-    function handleCanvas() {
-      return new Canvas();
-    }
+    // function handleCanvas() {
+    //   return new Canvas();
+    // }
 
-    if (!isMobile) handleCanvas();
+    // if (!isMobile && pathname === '/') handleCanvas();
+    // else return () => window.removeEventListener('load', handleCanvas);
 
-    return () =>
-      window.removeEventListener('load', () => {
-        if (isMobile) {
-          handleCanvas();
-        }
-      });
+    // return () => window.removeEventListener('load', handleCanvas);
   }, []);
 
   return (
@@ -68,38 +70,35 @@ function App() {
         className={!pathname.includes('admin') ? style.logo : style.adminlogo}
         alt='로고'
       />
-      <div>
+      <div className={style.contentsWrap}>
         <Navigation />
         <ConnectedRouter history={history}>
-          <ScrollRestoration></ScrollRestoration>
+          <ScrollRestoration />
           <Route path='/' exact component={Home} />
           <Route path='/search' exact component={Search} />
           <Route path='/contents' exact component={Contents} />
           <Route path='/detail/:id' exact component={Detail} />
+          <Route path='/chatting' exact component={Chatting} />
+          <Route path='/chatroom/:id' exact component={ChatRoom} />
+          <Route path='/mbti/test' exact component={MbtiTest} />
+          <Route path='/mbti/result/:id' exact component={MbtiTestResult} />
+          <Route path='/userpage' exact component={UserPage} />
+          <Route path='/useredit' exact component={ProfileEdit} />
+          <Route path='/notification' exact component={Notification} />
+          <Route path='/alarm' exact component={Alarm} />
+          <Route path='/termsofuse' exact component={TermsOfUse} />
           <Route path='/signup' exact component={SignUp} />
           <Route path='/signin' exact component={SignIn} />
-          <Route path='/signin_email' exact component={EmailSignIn} />
+          <Route path='/signin_email' exact component={SignInEmail} />
           <Route path='/signin_findpwd' exact component={FindPwd} />
           <Route path='/admin' exact component={Admin} />
-          <Route path='/userpage' exact component={Setting} />
-          <Route path='/useredit' exact component={ProfileEdit} />
           <Route path='/admin_write' exact component={AdminPostManage} />
           <Route path='/admin_write/:id' exact component={AdminPostManage} />
           <Route path='/admin_preview' exact component={AdminPostPreview} />
           <Route path='/admin_preview/:id' exact component={AdminPostPreview} />
-          <Route path='/notification' exact component={Notification} />
-          <Route path='/alarm' exact component={Alarm} />
-          <Route path='/termsofuse' exact component={TermsofUse} />
-          <Route path='/chatting' exact component={Chatting} />
-          <Route path='/chatroom/:id' exact component={ChatRoom} />
-          <Route path='/compatibility' exact component={Compatibility} />
-          <Route
-            path='/compatibility/result/:id'
-            exact
-            component={CompatibilityResult}
-          />
         </ConnectedRouter>
       </div>
+      <div className={style.bg}></div>
     </div>
   );
 }
