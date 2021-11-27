ed5a85598c07b6feb9ef9010c2fc30806d24c062
Asher <grit.developer.asher@gmail.com>
2021-11-27 19:32:43 +0900

    Edit. 준비중 멘트 추가

--- src/pages/Alarm/index.jsx
+++ src/pages/Alarm/index.jsx
@@ -6,6 +6,19 @@ const Alarm = () => {
   return (
     <div>
       <Goback>알림설정</Goback>
+      <p
+        style={{
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: 'translateX(-50%) translateY(-50%)',
+          fontSize: '30px',
+          textAlign: 'center',
+          color: '#999',
+        }}
+      >
+        준비 중 입니다.
+      </p>
     </div>
   );
 };
--- src/pages/Notification/index.jsx
+++ src/pages/Notification/index.jsx
@@ -6,6 +6,19 @@ const Notification = () => {
   return (
     <div>
       <Goback page='/userpage'>공지사항</Goback>
+      <p
+        style={{
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: 'translateX(-50%) translateY(-50%)',
+          fontSize: '30px',
+          textAlign: 'center',
+          color: '#999',
+        }}
+      >
+        준비 중 입니다.
+      </p>
     </div>
   );
 };
