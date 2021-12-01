48cc01e5bc53c462367980d23f67f6324e0e589b
Asher <grit.developer.asher@gmail.com>
2021-12-01 17:23:34 +0900

    Edit. 상황별 디테일 페이지에서 상황별 mbti에 맞게 표시

--- src/components/MbtiReaction/index.jsx
+++ src/components/MbtiReaction/index.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import classnames from 'classnames';
 // css
 import style from './mbtiReaction.module.css';
 
 const MbtiReaction = props => {
+  const userMbti = JSON.parse(
+    sessionStorage.getItem('user'),
+  ).user_mbti.toLowerCase();
+
   return (
     <div className={style.wrap}>
       <div className={style.box}>
-        <div className={style.name}>{props[0]?.toUpperCase()}</div>
-        <div className={style.desc}>{props[1]}</div>
+        <div
+          className={classnames(
+            style.name,
+            props[0].toLowerCase() === userMbti && style.target,
+          )}
+        >
+          {props[0]?.toUpperCase()}
+        </div>
+        <div
+          className={classnames(
+            style.desc,
+            props[0].toLowerCase() === userMbti && style.target,
+          )}
+        >
+          {props[1]}
+        </div>
       </div>
     </div>
   );
