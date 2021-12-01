d78a7fe6a487e7e20d7d972ebde1abc099c04bb2
Asher <grit.developer.asher@gmail.com>
2021-12-01 16:11:52 +0900

    Edit. soket end point 주소 환경변수로 설정

--- src/shared/socket.js
+++ src/shared/socket.js
@@ -1,5 +1,4 @@
 import io from 'socket.io-client';
-// http://54.180.157.153
-// wss://stravinest.shop
-const EndPoint = 'wss://stravinest.shop';
+
+const EndPoint = process.env.REACT_APP_SOKET_END_POINT;
 export const socket = io(EndPoint, { transports: ['websocket'] });
